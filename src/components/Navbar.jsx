287405d87515b0301b9d3790ffbb09f8a915d0cd
Quaintbrake5 <denzylibe6@gmail.com>
2024-06-24 12:49:00 +0100

    Added my project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <section className='navbar' id='navigation'>
+        
+      </section>
+    </>
+  )
+}
+
+export default Navbar
