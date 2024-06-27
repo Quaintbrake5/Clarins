f0635904f5ff5d01cf33d027f0028e174b0c563d
Quaintbrake5 <denzylibe6@gmail.com>
2024-06-27 01:04:58 +0100

    Added my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/Header'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Header/>
     </>
   )
 }
